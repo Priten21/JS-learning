d5c2259812adeb8dffaacd6d27ce8dc6dd6937be
Priten Thakkar <154042239+Priten21@users.noreply.github.com>
2025-10-29 04:59:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node JS nt browser1
+
+console.log(3 + 3)
+console.log("Priten") // code readability should be high
+
+
+let name = "Priten"  //String
+let age = 21 // Number , bigint for very large numbers
+let isLoggeddIn = false //boolean 
+let state = null // null => standalone value 
+// undefined
+// Symbol => unique
+
+// object 
+console.log(typeof null); // object
+console.log(typeof undefined)
