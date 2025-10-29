350852174b7b841e41987da44cec85b4ac3cd948
Priten Thakkar <154042239+Priten21@users.noreply.github.com>
2025-10-29 05:03:07 +0000

    vaariables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12445
+let accountEmail = "Priten@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 not allowed to change const
+console.log(accountId);
+
+accountEmail = "pritenthakkar@gmail.com"
+accountPassword = 12334555
+accountCity = "ahmedabad"
+console.log(accountEmail);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
