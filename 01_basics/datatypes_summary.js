9775464028d71047b90ae0d2e88bbdebc6bb962f
Priten Thakkar <154042239+Priten21@users.noreply.github.com>
2025-10-29 07:18:54 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,35 @@
+//Primitive : (Value)
+
+// 7 types : String , Number , Boolean , null ,undefined , Symbol , bigint 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symnol('123')
+
+const anotherId = Symbol('123')
+
+log(id == anotherId)
+
+// Non Primitive : (Reference)
+
+// Array , Objects , Functions 
+
+const bigint =3333232837238623623283632n
+
+const heros = ["Shaktiman","Spiderman","Batman"];
+
+let myObj = {
+    name : "Priten",
+    age : 21
+}
+
+
+const myFunc = function(){
+    console.log("hello world")
+}
+
