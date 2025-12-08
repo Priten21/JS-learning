74d7fcb1d4b702db16e286fdcce5f68dbaeb6bf6
Priten Thakkar <154042239+Priten21@users.noreply.github.com>
2025-12-08 18:51:39 +0530

    Node.JS
    FS & HTTP module of node.js

---
+++ 09_backend/basics.js
@@ -0,0 +1,47 @@
+// let n = 5;
+
+// for (let i =0;i<n;i++) {
+//     console.log("hello,",i);
+    
+// }
+
+
+// process :This  object provides information about, and control over, the current node.js process
+
+// process.argv : returns an array containing the command line arguments passed when the node.js process was lauanched 
+
+
+// let args = process.argv;
+
+// for(let i=2; i < args.length;i++) {
+//     console.log("hello and welcome ",args[i]);
+// }
+
+
+
+// ========getting methods and values from other files; ==========
+
+
+// module.exports ---> require() , if module.exports is empty it returns an empty object.
+
+// const math = require("./math");
+
+
+// console.log(math.sum(2,3));
+// console.log(math.g);
+
+
+// =========accesing the data from fruits directory=========
+
+
+// const info = require("./Fruits");
+
+// console.log(info[0].name);
+ 
+
+
+// ======== npm ========= 
+ 
+
+// npm(node package manager ) : npm is the standard package manager for node.js.... it contains the package we can use in our program...along with it npm ois a command line tool through which we can install the packages and chhose versions to install.
+
